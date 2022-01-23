21b4cac7170ef09404909097c2831127be9ad8e0
gozde <gozdecengiiz98@gmail.com>
2022-01-23 14:56:35 +0300

    all commit

---
+++ NewsletterProject/Extensions/ActionControlExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using NewsletterProject.Middlewares;
+
+namespace NewsletterProject.Extensions
+{
+    static public class ActionControlExtension
+    {
+        public static IApplicationBuilder UseActionControl(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<ActionControlMiddleware>();
+        }
+    }
+}
---
+++ NewsletterProject/Middlewares/ActionControlMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace NewsletterProject.Middlewares
+{
+    public class ActionControlMiddleware
+    {
+        RequestDelegate _next;
+        public ActionControlMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        
+        public async Task Invoke(HttpContext httpContext)
+        {
+            Console.WriteLine("Actıona gırıs yapıldı");
+            await _next.Invoke(httpContext);
+            Console.WriteLine("Actıondan cıkıs yapıldı");
+
+        }
+    }
+}
